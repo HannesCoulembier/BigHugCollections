6661705cc28758278624c85ba41f166317bf5682
Hannes Coulembier <hannes.coulembier@gmail.com>
2024-11-25 22:36:15 +0100

    Started Set class

--- Python/sandbox.py
+++ Python/sandbox.py
@@ -1,10 +1,27 @@
 import Tools.color as color
 import Tools.io as io
+import Tools.bhmath as math
 
-import os
+empty = math.Set(math.Sets.Empty)
 
-print(os.getcwd())
+oneTwo = math.Set({1,2})
+condOneTwo = math.Set({1,2}, [lambda x: x%2==0])
 
-file = io.File("Python\sadbox.py")
-print(file.exists())
-print(file.readStr())
+all = math.Set(math.Sets.All)
+allCond = math.Set(math.Sets.All, [lambda x: x%2==0])
+
+finGen = math.Set(range(10))
+finGenCond = math.Set(range(10), [lambda x: x%2==0])
+for e in condOneTwo:
+    print(e)
+for e in condOneTwo:
+    print(e)
+
+
+# import os
+
+# print(os.getcwd())
+
+# file = io.File("Python\sadbox.py")
+# print(file.exists())
+# print(file.readStr())
